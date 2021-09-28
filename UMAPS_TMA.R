39b391a6e7a00d1ddd105eba5049d52b7c457a1d
Inga-Maria <54940812+Inga-Maria@users.noreply.github.com>
2021-09-28 14:14:20 +0300

    Update UMAPS_TMA.R

--- UMAPS_TMA.R
+++ UMAPS_TMA.R
@@ -4,20 +4,19 @@ library(viridis)
 library(ggplot2)
 
 
-all_celltypes <- read.csv("TMA_annotated_single_cell_data.csv")
+#all_celltypes <- read.csv("TMA_annotated_single_cell_data.csv")
 
+all_celltypes <- read.csv("global_data_TMA.csv")
+all_celltypes_immune <- read.csv("immune_data_TMA.csv")
+all_celltypes_tumor <- read.csv("tumor_data_TMA.csv")
+all_celltypes_stroma <- read.csv("stromal_data_TMA.csv")
 
 #################################################################################################################
 ################   GLOBAL CELL TYPE UMAPS #######################################################################
 
 df <- all_celltypes[, c("Sample","CD163","CD20","CD4","CD3d","CD8a", "CD45", "pSTAT1", "P21", "Ki67", "PDL1", "PD1", "cCasp3",
-                                 "FOXP3","IBA1","CK7","CD11c","Ecadherin","vimentin", "CD31","Subtype", "GlobalCellType")]
+                                 "FOXP3","IBA1","CK7","CD11c","Ecadherin","vimentin", "CD31","Subtype")]
 
-
-df$Subtype <- as.character(df$Subtype)
-
-df[which(df$GlobalCellType == "Endothelia"), "Subtype"] <- "Endothelia"
-df[which(df$Subtype == "Stroma_Endothelia"), "Subtype"] <- "Stroma"
 
 df$Subtype <- as.factor(df$Subtype)
 #2:14
@@ -65,29 +64,29 @@ print(p1)
 ##############################################################################################################
 ##################### IMMUNE CELL UMAPS ######################################################################
 
-all_celltypes_24092020 <- read.csv("TMA_annotated_single_cell_data.csv")
+#all_celltypes_24092020 <- read.csv("TMA_annotated_single_cell_data.csv")
 
-all_celltypes_24092020$GlobalCellType <- as.character(all_celltypes_24092020$GlobalCellType)
+all_celltypes_24092020 <- all_celltypes_immune
+all_celltypes_24092020$Subtype <- as.character(all_celltypes_24092020$Subtype)
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD11c+CD163+IBA1+Macrophages"), "Subtype"] <- "IBA1+CD163+CD11c+ Macrophages"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD11c+IBA1+Macrophages"), "Subtype"] <- "IBA1+CD11c+ Macrophages"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD163+IBA1+Macrophages"), "Subtype"] <- "IBA1+CD163+ Macrophages"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD4"), "Subtype"] <- "CD4+ Effector T-cells"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD8"), "Subtype"] <- "CD8+ T-cells"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD163+Macrophages"), "Subtype"] <- "CD163+ Macrophages"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "IBA1+Macrophages"), "Subtype"] <- "IBA1+ Macrophages"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "CD11c+APC"), "Subtype"] <- "CD11c+APC"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "FOXP3+CD4+Tregs"), "Subtype"] <- "FOXP3+CD4+ T-regs"
 
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD11c+CD163+IBA1+Macrophages"), "GlobalCellType"] <- "IBA1+CD163+CD11c+ Macrophages"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD11c+IBA1+Macrophages"), "GlobalCellType"] <- "IBA1+CD11c+ Macrophages"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD163+IBA1+Macrophages"), "GlobalCellType"] <- "IBA1+CD163+ Macrophages"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD4"), "GlobalCellType"] <- "CD4+ Effector T-cells"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD8"), "GlobalCellType"] <- "CD8+ T-cells"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD163+Macrophages"), "GlobalCellType"] <- "CD163+ Macrophages"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "IBA1+Macrophages"), "GlobalCellType"] <- "IBA1+ Macrophages"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "CD11c+APC"), "GlobalCellType"] <- "CD11c+APC"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "FOXP3+CD4+Tregs"), "GlobalCellType"] <- "FOXP3+CD4+ T-regs"
 
-
-all_cell_types_16092020_immune <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Immune"),]
+#all_cell_types_16092020_immune <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Immune"),]
 
 df <- all_cell_types_16092020_immune[, c("Sample","CD163","CD20","CD4","CD3d","CD8a",
-                                         "FOXP3","IBA1","CD11c", "CD1c", "GlobalCellType")]
-df$GlobalCellType <- as.factor(df$GlobalCellType)
+                                         "FOXP3","IBA1","CD11c", "CD1c", "Subtype")]
+df$Subtype <- as.factor(df$Subtype)
 
 
-cell_type <- df$GlobalCellType
+cell_type <- df$Subtype
 
 umap_s = uwot::umap(df[,c(2:10)],n_neighbors = 80, scale=T ,spread=2.5,n_epochs = 60, min_dist = 0.2)
 uplot = data.frame(x = umap_s[,1], y= umap_s[,2])
@@ -114,23 +113,24 @@ for (i in c(2:10)){
 ################################################################################################################
 ####################### STROMAL CELL UMAPS #####################################################################
 
-all_celltypes_24092020 <- read.csv("TMA_annotated_single_cell_data.csv")
-all_celltypes_24092020$GlobalCellType <- as.character(all_celltypes_24092020$GlobalCellType)
+#all_celltypes_24092020 <- read.csv("TMA_annotated_single_cell_data.csv")
 
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "High-PDL1"), "GlobalCellType"] <- "Functional stroma"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "Non-proliferative_Stroma"), "GlobalCellType"] <- "Non-proliferative Stroma"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "Low_eccentricity_medium_vimentin"), "GlobalCellType"] <- "Low eccentricity"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "Proliferative_Stroma"), "GlobalCellType"] <- "Proliferative Stroma"
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "High-proliferative_Stroma"), "GlobalCellType"] <- "High-proliferative Stroma"
+all_celltypes_24092020 <- all_celltypes_stroma
+all_celltypes_24092020$Subtype <- as.character(all_celltypes_24092020$Subtype)
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "High-PDL1"), "Subtype"] <- "Functional stroma"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Non-proliferative_Stroma"), "Subtype"] <- "Non-proliferative Stroma"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Low_eccentricity_medium_vimentin"), "Subtype"] <- "Low eccentricity"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Proliferative_Stroma"), "Subtype"] <- "Proliferative Stroma"
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "High-proliferative_Stroma"), "Subtype"] <- "High-proliferative Stroma"
 
-all_cell_types_13102020_stromal <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Stroma_Endothelia"),]
+#all_cell_types_13102020_stromal <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Stroma_Endothelia"),]
 
-df <- all_cell_types_13102020_stromal[, c("Sample","cCasp3", "pSTAT1", "Ki67",
-                                          "PDL1","vimentin","CD31","P21","Eccentricity", "GlobalCellType")]
-df$GlobalCellType <- as.factor(df$GlobalCellType)
+df <- all_celltypes_24092020[, c("Sample","cCasp3", "pSTAT1", "Ki67",
+                                          "PDL1","vimentin","CD31","P21","Eccentricity", "Subtype")]
+df$Subtype <- as.factor(df$Subtype)
 
 
-cell_type <- df$GlobalCellType
+cell_type <- df$Subtype
 umap_s = uwot::umap(df[,c(2:9)],n_neighbors = 100, scale=T ,spread=1.5,n_epochs = 50)
 uplot = data.frame(x = umap_s[,1], y= umap_s[,2])
 mycolors <- colorRampPalette(brewer.pal(9, "Set3"))(9)
@@ -158,20 +158,22 @@ for (i in c(2:9)){
 ##################################################################################################################
 ########################## TUMOR CELL UMAPS ######################################################################
 
-read.csv("TMA_annotated_single_cell_data.csv")
-all_celltypes_24092020$GlobalCellType <- as.character(all_celltypes_24092020$GlobalCellType)
+#read.csv("TMA_annotated_single_cell_data.csv")
 
-all_celltypes_24092020[which(all_celltypes_24092020$GlobalCellType == "Hyperfunctional epithelial"), "GlobalCellType"] <- "Functional epithelial"
+all_celltypes_24092020 <- all_celltypes_tumor
+all_celltypes_24092020$Subtype <- as.character(all_celltypes_24092020$Subtype)
 
-all_cell_types_13102020_tumor <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Tumor"),]
-all_cell_types_13102020_tumor <- all_cell_types_13102020_tumor[-which(all_cell_types_13102020_tumor$GlobalCellType == "Negative"),]
+all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Hyperfunctional epithelial"), "Subtype"] <- "Functional epithelial"
 
-df <- all_cell_types_13102020_tumor[, c("Sample","cCasp3", "pSTAT1", "Ki67",
-                                        "PDL1","vimentin","Ecadherin","P21", "GlobalCellType")]
-df$GlobalCellType <- as.factor(df$GlobalCellType)
+#all_cell_types_13102020_tumor <- all_celltypes_24092020[which(all_celltypes_24092020$Subtype == "Tumor"),]
+#all_cell_types_13102020_tumor <- all_cell_types_13102020_tumor[-which(all_cell_types_13102020_tumor$GlobalCellType == "Negative"),]
+
+df <- all_celltypes_24092020[, c("Sample","cCasp3", "pSTAT1", "Ki67",
+                                        "PDL1","vimentin","Ecadherin","P21", "Subtype")]
+df$Subtype <- as.factor(df$Subtype)
 
 
-cell_type <- df$GlobalCellType
+cell_type <- df$Subtype
 
 umap_s = uwot::umap(df[,c(2:8)],n_neighbors = 80, scale=T ,spread=0.7,n_epochs = 30)
 uplot = data.frame(x = umap_s[,1], y= umap_s[,2])
